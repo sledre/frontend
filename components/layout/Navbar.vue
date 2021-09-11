3d872314a97e22328c3229ef90ecb37c5716da0c
Kn0wl3dge <baptistin.boilot@epita.fr>
2021-09-11 18:32:08 +0200

    Rename AutoDetours to SledRE

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -2,7 +2,7 @@
   <b-navbar fixed="top" class="navbar-dark bg-secondary shadow-lg">
     <nuxt-link class="navbar-brand" to="/">
       <img src="~/assets/img/logo-small.png" height="30" class="mr-2 pb-1">
-      AutoDetours
+      SledRE
     </nuxt-link>
   </b-navbar>
 </template>
